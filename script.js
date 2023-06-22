6b1ed878d55837ce0f0a8ee70e14f8d6d756a0e3
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2023-06-22 08:13:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,18 +31,18 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(str) {
+function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-	for(let i=0; i<str.length; i++) {
-		if(lookup[str[i]] === undefined) {
-			decodedArr.push(str[i]);
+	for(let i=0; i<encodedStr.length; i++) {
+		if(lookup[encodedStr[i]] == undefined) {
+			decodedArr.push(encodedStr[i]);
 		}
 		else {
-			decodedArr.push(lookup[str[i]])
+			encodedStr.push(lookup[encodedStr[i]])
 		}
 	}
-	return decodedArr;
+	return decodedArr.join("");
 
   return; //return decodedArr
 }
